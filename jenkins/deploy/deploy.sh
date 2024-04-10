5ae41380ddaba00f164194f352ff95c12dc65647
diego <diego@diego.com>
2024-04-10 23:47:25 +0000

    forgot some ips

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,5 +5,5 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth prod-user@192.168.0.16:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@192.168.0.20:/tmp/publish
-ssh -i /opt/prod prod-user@192.168.0.20 "/tmp/publish"
+scp -i /opt/prod ./jenkins/deploy/publish prod-user@192.168.0.16:/tmp/publish
+ssh -i /opt/prod prod-user@192.168.0.16 "/tmp/publish"
