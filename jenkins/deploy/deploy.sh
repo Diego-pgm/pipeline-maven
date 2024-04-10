0095af1d08194b337c3576911cacd9334c49bbdb
diego <diego@diego.com>
2024-04-10 23:42:40 +0000

    changed the host because pipeline failed

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@192.168.0.20:/tmp/.auth
+scp -i /opt/prod /tmp/.auth prod-user@192.168.0.16:/tmp/.auth
 scp -i /opt/prod ./jenkins/deploy/publish prod-user@192.168.0.20:/tmp/publish
 ssh -i /opt/prod prod-user@192.168.0.20 "/tmp/publish"
